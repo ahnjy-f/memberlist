7cc15618df2a071248c1deb8cea0a81f0b3465bf
Dong <dlehddus1285@gmail.com>
2020-05-27 07:16:04 +0000

    fix/6796

--- app/controllers/account/sessions_controller.rb
+++ app/controllers/account/sessions_controller.rb
@@ -50,8 +50,7 @@ class Account::SessionsController < Account::Base
         # @members=Member.order(created_at: :desc);
         # @members=Member.where(posts:!nil).order(created_at: :desc);
         # pp @members=Member.joins(:posts).preload(:posts).where("posts IS NOT NULL").distinct.order(created_at: :desc);
-        @members=Member.includes(:posts).references(:posts).order(created_at: :desc);
-        @p=Post.order(created_at: :desc);
+        @post=Post.order(created_at: :desc);
         @m=current_account;
         @current_account = current_account;
         @current_admin = current_admin;
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,6 +1,7 @@
 class Reply < ApplicationRecord
   belongs_to :member, class_name: "Member", foreign_key: "member_id", optional: true
   belongs_to :post, class_name: "Post", foreign_key: "post_id", optional: true
+  has_many :replylikes,class_name: "Replylike",dependent: :destroy
 
   accepts_nested_attributes_for :member
 end
